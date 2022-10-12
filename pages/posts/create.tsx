45b69327f8b055753cb302f274bc9d2e0d4a0602
Đào Văn Đạt <datdv@its-global.vn>
2022-10-12 11:36:00 +0700

    NextJS: 02-02 File-system Routing

---
+++ pages/posts/create.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export interface CreatePostProps {
+}
+
+export default function CreatePostPage (props: CreatePostProps) {
+  return (
+    <div>
+      Create Post Page
+    </div>
+  );
+}
