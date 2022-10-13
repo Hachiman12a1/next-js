63ad71785da61c8e2cb386dd26da363c2c1802fc
Đào Văn Đạt <datdv@its-global.vn>
2022-10-13 11:54:17 +0700

    NextJS: 02-08 SSG and Data Fetching on client side

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,23 +1,71 @@
-import { useRouter } from 'next/router';
-import  React from 'react';
+import Header from "@/components/common/header";
+// import dynamic from "next/dynamic";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
 
-export interface AboutProps {
-}
+// const Header = dynamic(() => import("@/components/common/header"), {
+//   ssr: false,
+// });
 
-export default function App (props: AboutProps) {
+export interface AboutProps {}
+
+export default function About(props: AboutProps) {
+  const [postList, setPostList] = useState([]);
   const router = useRouter();
 
-  console.log('About query : ', router.query);
+  console.log("About query : ", router.query);
+  const page = router.query?.page;
+
+  useEffect(() => {
+    if (!page) return;
+
+    (async () => {
+      const response = await fetch(
+        `https://js-post-api.herokuapp.com/api/posts?_page=${page}`
+      );
+      const data = await response.json();
+
+      setPostList(data.data);
+    })();
+  }, [page]);
+
+  const handleNextClick = () => {
+    router.push(
+      {
+        pathname: "/about",
+        query: {
+          page: (Number(page) || 1) + 1,
+        },
+      },
+      undefined,
+      { shallow: true }
+    );
+  };
 
   return (
     <div>
-      About Page
+      <h1>About Page</h1>
+
+      <Header />
+      <ul className="post-list">
+        {postList.map((post: any) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+      <button onClick={handleNextClick}>Next Page</button>
     </div>
   );
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
+  console.log("get static props");
+
   return {
-    props: {}, // will be passed to the page component as props
-  }
+    props: {},
+  };
 }
+// export async function getServerSideProps() {
+//   return {
+//     props: {}, // will be passed to the page component as props
+//   };
+// }
