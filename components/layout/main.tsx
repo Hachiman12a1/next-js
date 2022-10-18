f9b34477896ceec3ec668e7eb60c44115e20bcce
Đào Văn Đạt <datdv@its-global.vn>
2022-10-18 15:46:37 +0700

    NextJS: 04-10 Setup font-family for MUI 5

--- components/layout/main.tsx
+++ components/layout/main.tsx
@@ -1,9 +1,7 @@
 import { LayoutProps } from "@/models/index";
-import Link from "next/link";
-import * as React from "react";
 import { Stack } from "@mui/material";
+import { Box } from "@mui/system";
 import { Footer, Header } from "../common";
-import { Box, Container } from "@mui/system";
 
 export function MainLayout({ children }: LayoutProps) {
   return (
@@ -11,34 +9,6 @@ export function MainLayout({ children }: LayoutProps) {
       <Header />
 
       <Box component="main" flexGrow={1}>
-        <Container
-          maxWidth="sm"
-          sx={{
-            bgcolor: "primary.main",
-          }}
-        >
-          SM CONTAINER
-        </Container>
-
-        <Container
-          sx={{
-            bgcolor: "primary.main",
-          }}
-        >
-          MD CONTAINER
-        </Container>
-
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-
-        <Link href="/blog">
-          <a>Blog</a>
-        </Link>
-
-        <Link href="/works">
-          <a>Works</a>
-        </Link>
         {children}
       </Box>
 
